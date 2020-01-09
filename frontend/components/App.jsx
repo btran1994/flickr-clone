621dbf900024c02a7a9138ae6a46f2bcf91393fd
Brian <xbrianxtranx@gmail.com>
2020-01-08 16:20:46 -0800

    update most of user auth done

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,18 +7,60 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div className="splash-main">
-        <div className="main">
+        {/* <div className="main"> */}
             <header className="splash-header">
                 <h1>Stuttr?</h1>
                 <Route exact path='/' component={SplashContainer} />
             </header>
-        </div>
+        {/* </div> */}
         <div className="form-container">
             <Switch>
                 <AuthRoute path="/login" component={LoginFormContainer} />
                 <AuthRoute path="/signup" component={SignupFormContainer} />
             </Switch>
+            <ul className="cb-slideshow">
+                <li>
+                    <span>Image 01</span>
+                </li>
+                <li>
+                    <span>Image 02</span>
+                </li>
+                <li>
+                    <span>Image 03</span>
+                </li>
+                <li>
+                    <span>Image 04</span>
+                </li>
+                <li>
+                    <span>Image 05</span>
+                </li>
+                <li>
+                    <span>Image 06</span>
+                </li>
+                <li>
+                    <span>Image 07</span>
+                </li>
+                <li>
+                    <span>Image 08</span>
+                </li>
+                <li>
+                    <span>Image 09</span>
+                </li>
+                <li>
+                    <span>Image 10</span>
+                </li>
+                <li>
+                    <span>Image 11</span>
+                </li>
+                <li>
+                    <span>Image 12</span>
+                </li>
+            </ul>
+            <footer className="footer">
+                <a href="https://github.com/btran1994" className="git-link"><img src="https://www.iconsdb.com/icons/preview/white/github-11-xxl.png"/></a>
+            </footer>
         </div>
+        
     </div>
             
 );
